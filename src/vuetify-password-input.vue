0e41dfb3ea84fc8ba7dcddec0633a7f93a75e3e3
pytness <pytness@gmail.com>
2021-08-16 16:21:45 +0200

    Updates main component

--- src/vuetify-password-input.vue
+++ src/vuetify-password-input.vue
@@ -1,88 +1,126 @@
-<script lang="ts">
-import Vue from 'vue';
-
-interface SampleData {
-  counter: number;
-  initCounter: number;
-  message: {
-    action: string | null;
-    amount: number | null;
-  };
-}
-
-export default /*#__PURE__*/Vue.extend({
-  name: 'VuetifyPasswordInput', // vue component name
-  data(): SampleData {
-    return {
-      counter: 5,
-      initCounter: 5,
-      message: {
-        action: null,
-        amount: null,
-      },
-    };
-  },
-  computed: {
-    changedBy() {
-      const { message } = this as SampleData;
-      if (!message.action) return 'initialized';
-      return `${message.action} ${message.amount || ''}`.trim();
-    },
-  },
-  methods: {
-    increment(arg: Event | number): void {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
-      this.counter += amount;
-      this.message.action = 'incremented by';
-      this.message.amount = amount;
-    },
-    decrement(arg: Event | number): void {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
-      this.counter -= amount;
-      this.message.action = 'decremented by';
-      this.message.amount = amount;
-    },
-    reset(): void {
-      this.counter = this.initCounter;
-      this.message.action = 'reset';
-      this.message.amount = null;
-    },
-  },
-});
-</script>
-
 <template>
-  <div class="vuetify-password-input">
-    <p>The counter was {{ changedBy }} to <b>{{ counter }}</b>.</p>
-    <button @click="increment">
-      Click +1
-    </button>
-    <button @click="decrement">
-      Click -1
-    </button>
-    <button @click="increment(5)">
-      Click +5
-    </button>
-    <button @click="decrement(5)">
-      Click -5
-    </button>
-    <button @click="reset">
-      Reset
-    </button>
-  </div>
+	<v-text-field
+		v-bind="$attrs"
+		:value="value"
+		v-on="$listeners"
+		@input="on_input"
+		@change="calc_strength(value)"
+		:type="show_password ? 'text' : 'password'"
+		:append-icon="show_password ? 'mdi-eye' : 'mdi-eye-off'"
+		@click:append="show_password = !show_password"
+		:loading="loading || show_strength"
+	>
+		<template #progress>
+			<v-progress-linear
+				v-if="loading"
+				indeterminate
+				absolute
+			/>
+			<password-strength
+				v-if="show_strength"
+				:class="loading ? 'mt-2' : ''"
+				v-model="strength"
+				:colors="colors"
+			/>
+		</template>
+	</v-text-field>
 </template>
 
-<style scoped>
-  .vuetify-password-input {
-    display: block;
-    width: 400px;
-    margin: 25px auto;
-    border: 1px solid #ccc;
-    background: #eaeaea;
-    text-align: center;
-    padding: 25px;
-  }
-  .vuetify-password-input p {
-    margin: 0 0 1em;
-  }
-</style>
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from "vue-property-decorator";
+import password_checker from "zxcvbn";
+
+import PasswordStrength from "@/components/password-strength.vue";
+
+@Component({
+	components: {
+		PasswordStrength,
+	},
+})
+export default class PasswordInput extends Vue {
+	@Prop({ default: "" })
+	public value!: string;
+
+	@Prop({ default: false })
+	public loading!: boolean;
+
+	@Prop({ default: false })
+	public show_strength!: boolean;
+
+	public show_password: boolean = false;
+
+	public strength: number = -1;
+
+	public colors: string[] = ["red", "orange", "yellow", "green"];
+
+
+	private calculate_strength(value: string): number {
+		const response = password_checker(value);
+		const guesses = response.guesses_log10;
+
+		console.log("response", response);
+
+		if (guesses === 0)
+			return -1;
+
+		const strength = Math.floor(
+			(guesses / 50) * this.colors.length
+		);
+		console.log("strength", strength);
+
+		return strength;
+	}
+
+	public on_input(value: string) {
+		this.$emit("input", value);
+	}
+
+	@Watch("value", { immediate: true })
+	public on_value_change(new_value: string, _: string) {
+		this.calc_strength(new_value);
+	}
+
+	@Watch("show_strength", { immediate: true })
+	public on_show_strength_change(new_value: boolean, _: string) {
+		if (new_value === true)
+			this.calc_strength(this.value);
+	}
+
+	public calc_strength(value: string) {
+		if (this.show_strength)
+			this.strength = this.calculate_strength(value);
+	}
+
+	public calc_entropy(password: string) {
+		if (password.length === 0)
+			return 0;
+
+		const count_map: Record<string, number> = {};
+
+		for (let char of password) {
+			if (char in count_map == false)
+				count_map[char] = 0;
+
+			count_map[char] += 1;
+		}
+
+		const probs = Object.values(count_map).map(
+			(count) => count / password.length
+		);
+
+		const entropy = probs
+			.map((p) => Math.log2(1 / p) / (1 / p))
+			.reduce((a, b) => a + b);
+
+		console.log("count map", count_map);
+		console.log("entropy", entropy);
+
+		return entropy;
+	}
+
+	mounted() {
+		// this.strength = this.calculate_strength(this.value);
+		// this.strength = this.calculate_strength(value);
+	}
+}
+</script>
