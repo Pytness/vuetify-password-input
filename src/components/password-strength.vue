1262d28d1c4774393e98581b680d8f9a771f11fb
pytness <pytness@gmail.com>
2021-08-16 16:21:25 +0200

    Added password-strength component

---
+++ src/components/password-strength.vue
@@ -0,0 +1,83 @@
+<template>
+	<div
+		role="progressbar"
+		class="v-progress-linear
+			v-progress-linear--absolute
+			v-progress-linear--rounded
+			v-progress-linear--visible"
+		style="height: 4px"
+	>
+		<v-row>
+			<v-col
+				v-for="(color, index) in colors"
+				:key="color"
+				:cols="12 / colors.length"
+				class="px-1"
+			>
+				<div
+					:class="get_bar_classes(index)"
+					style="height: 4px"
+				></div>
+			</v-col>
+
+		</v-row>
+	</div>
+</template>
+
+<script lang="ts">
+
+import {
+	Vue,
+	Component,
+	Prop,
+} from 'vue-property-decorator';
+
+
+@Component({})
+export default class PasswordStrength extends Vue {
+
+	@Prop({ default: -1 })
+	public value!: number;
+
+	@Prop({ default: 'blue-grey lighten-4' })
+	public backgroundColor!: string;
+
+	@Prop({
+		default: function () {
+			return [
+				'red',
+				'orange',
+				'amber',
+				'lime',
+				'light-green',
+				'green'
+			]
+		}
+	})
+	public colors!: string[];
+
+	public get_bar_classes(index: number) {
+		const color_index = this.value >= this.colors.length ?
+			this.colors.length - 1 : this.value;
+
+		return [
+			'v-progress-linear',
+			'progress-segment',
+			index <= this.value ?
+				this.colors[color_index]
+				: this.backgroundColor
+		]
+	}
+
+	public on_input(value: string) {
+		this.$emit('input', value);
+	}
+}
+</script>
+
+<style scoped>
+.progress-segment {
+	width: 100% !important;
+	position: initial !important;
+}
+</style>
