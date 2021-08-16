71e423381c3c7d04f6224236c76fae9796448539
pytness <pytness@gmail.com>
2021-08-16 12:31:28 +0200

    Initial commit. Executed sfc-init and installed packages.

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import VuetifyPasswordInput from '@/vuetify-password-input.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VuetifyPasswordInput
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vuetify-password-input />
+  </div>
+</template>
