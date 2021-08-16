07a158e5e42897a81c1850488dbbba1dd13604d8
pytness <pytness@gmail.com>
2021-08-16 12:54:26 +0200

    Formatted and Changed spaces for tabs

--- src/entry.esm.ts
+++ src/entry.esm.ts
@@ -10,14 +10,14 @@ type InstallableComponent = typeof component & PluginObject<any>;
 // IIFE injects install function into component, allowing component
 // to be registered via Vue.use() as well as Vue.component(),
 export default /*#__PURE__*/((): InstallableComponent => {
-  // Assign InstallableComponent type
-  const installable = component as unknown as InstallableComponent;
+	// Assign InstallableComponent type
+	const installable = component as unknown as InstallableComponent;
 
-  // Attach install function executed by Vue.use()
-  installable.install = (Vue: typeof _Vue) => {
-    Vue.component('VuetifyPasswordInput', installable);
-  };
-  return installable;
+	// Attach install function executed by Vue.use()
+	installable.install = (Vue: typeof _Vue) => {
+		Vue.component('VuetifyPasswordInput', installable);
+	};
+	return installable;
 })();
 
 // It's possible to expose named exports when writing components that can
