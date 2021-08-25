bf6bca4f0144597fa83d09fbc761be32fa6a93a8
pytness <pytness@gmail.com>
2021-08-25 13:16:58 +0200

    Added strength function option to the showcase. Fixed bug on rules input.

--- dev/serve.vue
+++ dev/serve.vue
@@ -22,6 +22,7 @@
 							:toggleable="toggleable"
 							:rules="selected_rules"
 							:show_strength="show_strength"
+							:strength_function="selected_strength_function"
 							:counter="show_counter"
 							:loading="loading"
 							:append-icon="selected_append_icon"
@@ -51,15 +52,20 @@
 							v-model="show_counter"
 							label="Show counter"
 						></v-checkbox>
-
-						<v-checkbox
-							v-model="show_strength"
-							label="Show strength"
-						></v-checkbox>
 						<v-checkbox
 							v-model="loading"
 							label="Loading"
 						></v-checkbox>
+						<v-checkbox
+							v-model="show_strength"
+							label="Show strength"
+						></v-checkbox>
+						<v-select
+							label="Strength Function"
+							v-model="selected_strength_f_key"
+							:items="strength_function_keys"
+							clearable
+						></v-select>
 						<v-slider
 							v-model="max_length"
 							thumb-label
@@ -67,10 +73,9 @@
 						></v-slider>
 						<v-select
 							label="Rules"
-							v-model="selected_rules"
-							:items="rules"
+							v-model="selected_rule_keys"
+							:items="rule_keys"
 							multiple
-							dense
 							clearable
 						></v-select>
 					</v-col>
@@ -89,13 +94,38 @@ import {
 } from 'vue-property-decorator';
 
 import PasswordInput from '@/vuetify-password-input.vue'
-import { DataTableHeader } from 'vuetify'
+
 
 type Rule = (value: string) => boolean | string;
-interface RuleTableHeader<T = any> extends Omit<DataTableHeader<T>, 'value'> {
-	value: Rule;
-};
+type StrengthFunction = (value: string) => number;
 
+
+function calc_entropy(password: string) {
+	if (password.length === 0)
+		return 0;
+
+	const count_map: Record<string, number> = {};
+
+	for (let char of password) {
+		if (char in count_map == false)
+			count_map[char] = 0;
+
+		count_map[char] += 1;
+	}
+
+	const probs = Object.values(count_map).map(
+		(count) => count / password.length
+	);
+
+	const entropy = probs
+		.map((p) => Math.log2(1 / p) / (1 / p))
+		.reduce((a, b) => a + b);
+
+	console.log("count map", count_map);
+	console.log("entropy", entropy);
+
+	return Math.round(entropy / 8 * 4);
+}
 
 @Component({
 	components: {
@@ -120,20 +150,40 @@ export default class Home extends Vue {
 	public loading: boolean = false;
 	public max_length: number = 0;
 
-	public selected_rules: Rule[] = [];
-	public rules: RuleTableHeader[] = [
-		{
-			text: 'Required',
-			value: (value: string) => {
-				return value.length > 0 || 'This field is required'
-			}
+	public rules: Record<string, Rule> = {
+		'Required': (value: string) => {
+			return value.length > 0 || 'This field is required'
 		},
-		{
-			text: 'Min Length',
-			value: (value: string) => {
-				return value.length > 8 || 'Not enough characters'
-			}
+		'Min length': (value: string) => {
+			return value.length > 8 || 'Not enough characters'
 		}
-	];
+	};
+
+	public strength_functions: Record<string, StrengthFunction> = {
+		'Shannon entropy': calc_entropy,
+		'Zxcvbn': (value: string) => value.length
+	};
+
+	public selected_rule_keys: string[] = [];
+	public selected_strength_f_key: string | null = null;
+
+	get rule_keys(): string[] {
+		return Object.keys(this.rules);
+	}
+
+	get strength_function_keys(): string[] {
+		return Object.keys(this.strength_functions);
+	}
+
+	get selected_rules(): Rule[] {
+		return this.selected_rule_keys.map(key => this.rules[key]);
+	}
+
+	get selected_strength_function(): StrengthFunction | null {
+		if (this.selected_strength_f_key !== null)
+			return this.strength_functions[this.selected_strength_f_key];
+
+		return null;
+	}
 }
 </script>
