37bd68b11aca3c1bf2d4e7980adf3f6f739bc39e
pytness <pytness@gmail.com>
2021-09-14 13:08:06 +0200

    Refactored install function as class method.

--- src/entry.esm.ts
+++ src/entry.esm.ts
@@ -1,25 +1,18 @@
-import _Vue, { PluginObject } from 'vue';
-
+import Vue from 'vue';
+import Vuetify, { VTextField } from 'vuetify/lib';
 // Import vue component
 import component from '@/vuetify-password-input.vue';
 
-// Define typescript interfaces for installable component
-type InstallableComponent = typeof component & PluginObject<any>;
+Vue.use(Vuetify, {
+	components: {
+		VTextField
+	}
+})
 
-// Default export is installable instance of component.
-// IIFE injects install function into component, allowing component
-// to be registered via Vue.use() as well as Vue.component(),
-export default /*#__PURE__*/((): InstallableComponent => {
-	// Assign InstallableComponent type
-	const installable = component as unknown as InstallableComponent;
+class Plugin {
+	public install(vue: typeof Vue, _?: any) {
+		vue.component('VPasswordInput', component);
+	}
+}
 
-	// Attach install function executed by Vue.use()
-	installable.install = (Vue: typeof _Vue) => {
-		Vue.component('VuetifyPasswordInput', installable);
-	};
-	return installable;
-})();
-
-// It's possible to expose named exports when writing components that can
-// also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
-// export const RollupDemoDirective = directive;
+export default new Plugin();
